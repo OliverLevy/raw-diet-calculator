c3c7f30b8ac22ee7c803209f1f310efcf93aade4
Oliver Levy <oliver.levy.ol@gmail.com>
2020-09-22 16:28:16 -0400

    added more functionality to display different conversions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   const [output, setOutput] = useState(null);
@@ -12,15 +12,15 @@ function App() {
     const weight = e.target.weight.value;
 
     if (age === "2") {
-      setOutput(`${Number(weight * 0.06)} lbs of raw meat per day`);
+      setOutput(Number(weight * 0.06).toFixed(2));
     } else if (age === "4") {
-      setOutput(`${Number(weight * 0.05)} lbs of raw meat per day`);
+      setOutput(Number(weight * 0.05).toFixed(2));
     } else if (age === "7") {
-      setOutput(`${Number(weight * 0.04)} lbs of raw meat per day`);
+      setOutput(Number(weight * 0.04).toFixed(2));
     } else if (age === "12" && activity === "moderate") {
-      setOutput(`${Number(weight * 0.02)} lbs of raw meat per day`);
+      setOutput(Number(weight * 0.02));
     } else {
-      setOutput(`${Number(weight * 0.03)} lbs of raw meat per day`);
+      setOutput(Number(weight * 0.03).toFixed(2));
     }
 
     console.log(e.target.age.value);
@@ -28,10 +28,27 @@ function App() {
     console.log(e.target.weight.value);
   };
 
-  const handleOutput = (value) => {
+  const createTable = (string) => {
+    const num = Number(string);
+    const lbsPerDay = num;
+    const lbsPerWeek = num * 7;
+    const lbsPerMonth = num * 30;
+    const boxPerMonth = ((lbsPerMonth / 40).toFixed(2));
+    const costPerMonth = boxPerMonth * 75;
+    const gramsPerDay = num * 454;
+    const gramsPerMeal2 = ((gramsPerDay / 2).toFixed(2));
+    const gramsPerMeal3 = ((gramsPerDay / 3).toFixed(2));
+
     return (
       <div>
-        <p>this is the return</p>
+        <p>{gramsPerDay} grams per day</p>
+        <p>{gramsPerMeal2} grams per meal if they eat twice a day</p>
+        <p>{gramsPerMeal3} grams per meal if they eat three times a day</p>
+        <p>{lbsPerDay} lbs per day</p>
+        <p>{lbsPerWeek} lbs per week</p>
+        <p>{lbsPerMonth} lbs per month</p>
+        <p>{boxPerMonth} box per month</p>
+        <p>${costPerMonth} per month</p>
       </div>
     );
   };
@@ -40,22 +57,34 @@ function App() {
     <div className="App">
       <form onSubmit={handleSubmit}>
         <h1>Raw Meat Calculator</h1>
-        <select name="age">
-          <option value="2">2 - 4 months</option>
-          <option value="4">4 - 7 months</option>
-          <option value="7">7 - 12 months</option>
-          <option value="12">12 +</option>
-        </select>
-
-        <select name="activity">
-          <option value="moderate">Moderate Activity</option>
-          <option value="high">High Activity</option>
-        </select>
-
-        <input type="number" name="weight" step="any" />
+        <div>
+          <label htmlFor="age">Dogs age</label>
+          <select name="age">
+            <option value="2">2 - 4 months</option>
+            <option value="4">4 - 7 months</option>
+            <option value="7">7 - 12 months</option>
+            <option value="12">12 +</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="activity">Activity Level</label>
+          <select name="activity">
+            <option value="moderate">Moderate Activity</option>
+            <option value="high">High Activity</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="weight">Your dog's weight in lbs</label>
+          <input
+            type="number"
+            name="weight"
+            step="any"
+            placeholder="weight in lbs"
+          />
+        </div>
         <button type="submit">Calculate</button>
       </form>
-      {output}
+      <h3>{createTable(output)}</h3>
     </div>
   );
 }
