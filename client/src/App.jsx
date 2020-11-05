615475169cac885fbbf391076e42abb2e119b399
Oliver Levy <oliver.levy.ol@gmail.com>
2020-11-04 21:13:20 -0500

    switched to 340 grams per cube instead of 460 grams

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
   const [output, setOutput] = useState(null);
+  const [isActive, setIsActive] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,74 +17,109 @@ function App() {
       setOutput(Number(weight * 0.05).toFixed(2));
     } else if (age === "7") {
       setOutput(Number(weight * 0.04).toFixed(2));
-    } else if (age === "12" && activity === "moderate") {
-      setOutput(Number(weight * 0.02));
+    } else if (age === "12" && weight <= 5) {
+      setOutput(Number(weight * 0.05).toFixed(2));
+    } else if (age === "12" && weight <= 10) {
+      setOutput(Number(weight * 0.04).toFixed(2));
+    } else if (age === "12" && weight < 15) {
+      setOutput(Number(weight * 0.03).toFixed(2));
+    } else if (age === "12" && weight >= 15 && activity === "moderate") {
+      setOutput(Number(weight * 0.02).toFixed(2));
     } else {
       setOutput(Number(weight * 0.03).toFixed(2));
     }
-
-    console.log(e.target.age.value);
-    console.log(e.target.activity.value);
-    console.log(e.target.weight.value);
   };
+
+  //add a bag price
 
   const createTable = (string) => {
     const num = Number(string);
-    const lbsPerDay = num;
-    const lbsPerWeek = num * 7;
-    const lbsPerMonth = num * 30;
-    const boxPerMonth = ((lbsPerMonth / 40).toFixed(2));
-    const costPerMonth = boxPerMonth * 75;
-    const gramsPerDay = num * 454;
-    const gramsPerMeal2 = ((gramsPerDay / 2).toFixed(2));
-    const gramsPerMeal3 = ((gramsPerDay / 3).toFixed(2));
+    // const lbsPerDay = num.toFixed(2);
+    // const lbsPerWeek = (num * 7).toFixed(2);
+    const lbsPerMonth = (num * 30).toFixed(2);
+    const boxPerMonth = (lbsPerMonth / 40).toFixed(2);
+    const costPerMonth = (boxPerMonth * 75).toFixed(2);
+    const gramsPerDay = (num * 454).toFixed(0);
+    const gramsPerMeal2 = (gramsPerDay / 2).toFixed(0);
+    const gramsPerMeal3 = (gramsPerDay / 3).toFixed(0);
+    const cubePerDay = (gramsPerDay / 340).toFixed(2);
+    const bagsPerMonth = (lbsPerMonth / 5).toFixed(2);
+    const costPerMonthBag = (bagsPerMonth * 12).toFixed(2);
 
     return (
       <div>
-        <p>{gramsPerDay} grams per day</p>
-        <p>{gramsPerMeal2} grams per meal if they eat twice a day</p>
-        <p>{gramsPerMeal3} grams per meal if they eat three times a day</p>
-        <p>{lbsPerDay} lbs per day</p>
-        <p>{lbsPerWeek} lbs per week</p>
-        <p>{lbsPerMonth} lbs per month</p>
-        <p>{boxPerMonth} box per month</p>
-        <p>${costPerMonth} per month</p>
+        <div className="calc-stats">
+          <h2>Daily</h2>
+          <p>{gramsPerDay} grams per day</p>
+          <p>{gramsPerMeal2} grams per meal (2 meals daily)</p>
+          <p>{gramsPerMeal3} grams per meal (3 meals daily)</p>
+          <p>{cubePerDay} blocks per day</p>
+        </div>
+        {/* <div className="calc-stats">
+          <p>{lbsPerDay} lbs per day</p>
+          <p>{lbsPerWeek} lbs per week</p>
+        </div> */}
+        <div className="calc-stats">
+          <h2>Monthly</h2>
+          <p>{lbsPerMonth} lbs per month</p>
+          <p>{boxPerMonth} box per month (40lbs)</p>
+          <p>{bagsPerMonth} bags per month (5lbs)</p>
+          <p>${costPerMonth} per month (box)</p>
+          <p>${costPerMonthBag} per month (bag)</p>
+        </div>
       </div>
     );
   };
 
+  const handleAge = (e) => {
+    if (e.target.value === "12") {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <h1>Raw Meat Calculator</h1>
-        <div>
-          <label htmlFor="age">Dogs age</label>
-          <select name="age">
+    <div className="raw-main">
+      <form onSubmit={handleSubmit} className="raw-main__form">
+        <div className="raw-main__input-container">
+          <label htmlFor="age">Dog's Age</label>
+          <select name="age" onChange={(e) => handleAge(e)}>
             <option value="2">2 - 4 months</option>
             <option value="4">4 - 7 months</option>
             <option value="7">7 - 12 months</option>
-            <option value="12">12 +</option>
+            <option value="12">12+ months</option>
           </select>
         </div>
-        <div>
-          <label htmlFor="activity">Activity Level</label>
-          <select name="activity">
+        <div
+          className="raw-main__input-container"
+          style={!isActive ? { display: "none" } : { display: "" }}
+        >
+          <label htmlFor="activity">Dog's Activity Level</label>
+          <select name="activity" disabled={!isActive}>
             <option value="moderate">Moderate Activity</option>
             <option value="high">High Activity</option>
           </select>
         </div>
-        <div>
-          <label htmlFor="weight">Your dog's weight in lbs</label>
+        <div className="raw-main__input-container">
+          <label htmlFor="weight">
+            {!isActive ? "Dog's Weight (lbs)" : "Dog's Ideal Weight (lbs)"}
+          </label>
           <input
             type="number"
             name="weight"
             step="any"
-            placeholder="weight in lbs"
+            placeholder={!isActive ? "weight in lbs" : "Ideal weight in lbs"}
           />
         </div>
-        <button type="submit">Calculate</button>
+        <div>
+          <button type="submit" className="raw-main__btn">
+            Calculate
+          </button>
+        </div>
       </form>
-      <h3>{createTable(output)}</h3>
+
+      {output && createTable(output)}
     </div>
   );
 }
